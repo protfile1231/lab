29b70aa07a2d032c7d1ca3e991db5f324cd3ed89
Skomarovski <Skomarovskiy.sergey@yandex.ru>
2020-04-19 21:03:10 +0700

    add Version.cs and change VersionController

--- Controllers/VersionController.cs
+++ Controllers/VersionController.cs
@@ -5,17 +5,21 @@ using lab.Models;
 namespace lab.Controllers
 {
     [Route("api/[controller]")]
-
     [ApiController]
-
     public class VersionController : ControllerBase
 
-    {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<string> Get()
-        {
-            return Ok(Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion);
-        }
-    }
+   {
+       // GET api/values
+       [HttpGet]
+       public ActionResult<string> Get()
+       {
+           var versionInfo = new Version
+           {
+               Company = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyCompanyAttribute>().Company,
+               Product = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyProductAttribute>().Product,
+               ProductVersion = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion
+           };
+           return Ok(versionInfo);
+       }
+   }
 }
